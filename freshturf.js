d52a9b2eb9861da119ab946311b5c0363c6972f1
Dmytro Kharchenko <unclenorton@gmail.com>
2018-10-04 18:17:59 +0800

    Cleanup the header

--- freshturf.js
+++ freshturf.js
@@ -1,16 +1,2 @@
 /*jslint browser: true */
 /*global document  */
-
-/*dropdown for Product */
-function drFunction() {
-    document.getElementById('dropdownMenu1').classList.toggle('show');
-}
-
-drFunction();
-
-/* dropdown for More */
-function drFn() {
-    document.getElementById('dropdownMenu2').classList.toggle('show');
-}
-
-drFn();
